d78db6234a91fed0c7793b7efdd49bf64e9689db
Marek <250497@wp.pl>
2018-04-17 01:06:14 +0200

    add error handling

--- pollution.erl
+++ pollution.erl
@@ -8,8 +8,8 @@
 %%%-------------------------------------------------------------------
 -module(pollution).
 -author("Prezes").
--export([createMonitor/0, addStation/3, addValue/5, testAdd/0, removeValue/4,
-  getOneValue/4, getStationMean/3, getDailyMean/3, getAreaMean/4, testRemove/0]).
+-export([createMonitor/0, addStation/3, addValue/5, removeValue/4,
+  getOneValue/4, getStationMean/3, getDailyMean/3, getAreaMean/4]).
 
 
 -record(measurements, {all = sets:new(),
@@ -24,6 +24,10 @@ createMonitor() -> #monitor{}.
 
 
 addStation(Name, {X,Y}, Monitor) ->
+  case dict:is_key(Name, (Monitor#monitor.stats)#stations.name_to_elem)
+  or dict:is_key({X, Y}, (Monitor#monitor.stats)#stations.coord_to_elem) of
+    true -> error(badarg);
+    false ->
   S = {Name, {X,Y}},
   Stats = (Monitor#monitor.stats),
   NewStats = Stats#stations
@@ -31,11 +35,15 @@ addStation(Name, {X,Y}, Monitor) ->
     coord_to_elem = dict:store({X, Y}, S, Stats#stations.coord_to_elem),
     name_to_elem = dict:store(Name, S, Stats#stations.name_to_elem)
   },
-  Monitor#monitor{stats = NewStats}.
+  Monitor#monitor{stats = NewStats}
+  end.
 
 
 addValue(StationInfo, Date, Type, Value, Monitor) ->
   Station = getStation(StationInfo, Monitor),
+  case dict:is_key({Type, Date, Station}, (Monitor#monitor.meas)#measurements.type_date_station_to_meas) of
+    true -> error(badarg);
+    false ->
   Measurement = {Station, Date, Type, Value},
   Meas = (Monitor#monitor.meas),
   NewMeas = Meas#measurements{
@@ -44,7 +52,8 @@ addValue(StationInfo, Date, Type, Value, Monitor) ->
     type_date_to_meas = dict:append({Type, element(1, Date)}, Measurement, Meas#measurements.type_date_to_meas),
     type_station_to_meas = dict:append({Type, Station}, Measurement, Meas#measurements.type_station_to_meas)
   },
-  Monitor#monitor{meas = NewMeas}.
+  Monitor#monitor{meas = NewMeas}
+  end.
 
 
 removeValue(StationInfo, Date, Type, Monitor) ->
